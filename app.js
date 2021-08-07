7bce01e3111df580baad181f7112a7fd6a757537
Nimesh Bansal <nimeshbansal22@gmail.com>
2021-08-08 02:48:28 +0530

    Daily Journal

---
+++ app.js
@@ -0,0 +1,224 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+
+const mongoose=require("mongoose");
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(session({
+  secret: "Our little secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb+srv://admin-Nimesh:nimesh1099@cluster0.zolfk.mongodb.net/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('useCreateIndex', true);
+
+const postSchema= new mongoose.Schema({
+  email:String,
+  password:String,
+  googleId:String,
+  title:String,
+  post:String
+});
+
+postSchema.plugin(passportLocalMongoose);
+postSchema.plugin(findOrCreate);
+
+const Post=mongoose.model("blogs",postSchema);
+
+passport.use(Post.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  Post.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/compose",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    Post.findOrCreate({
+      googleId: profile.id
+    }, function(err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+app.get("/", function(req, res) {
+  Post.find({
+    "post": {
+      $ne: null
+    }
+  }, function(err, founduser) {
+    if (err)
+      console.log(err);
+    else {
+      if (founduser) {
+        res.render("home", {
+          userPost: founduser,
+          content:homeStartingContent
+        });
+      }
+    }
+  });
+});
+
+app.get("/auth/google",
+  passport.authenticate('google', {
+    scope: ["profile"]
+  }));
+
+app.get("/auth/google/compose",
+  passport.authenticate('google', {
+    failureRedirect: "/login"
+  }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/compose");
+  });
+
+
+app.get("/about", function(req, res) {
+  res.render("about", {
+    about: aboutContent
+  });
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contact: contactContent
+  });
+});
+
+app.get("/compose", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("compose");
+  } else {
+    res.redirect("/signinUI");
+  }
+});
+
+app.post("/compose", function(req, res) {
+  const publishtitle=req.body.posttitle;
+  const publishpost=req.body.postbody;
+
+  Post.findById(req.user.id,function(err,foundUser){
+    if(err)
+    {
+      console.log(err);
+    }
+    else{
+      if(foundUser){
+        foundUser.title=publishtitle;
+        foundUser.post=publishpost;
+        foundUser.save(function(){
+          res.redirect("/");
+        })
+      }
+    }
+  });
+});
+
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+});
+
+app.get("/signinUI",function(req,res){
+  res.render("signinUI");
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+app.post("/register",function(req,res){
+  Post.register({
+    username: req.body.username
+  }, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate('local')(req, res, function() {
+        res.redirect("/");
+      });
+    }
+  });
+})
+
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.post("/login",function(req,res){
+  const user = new Post({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+      res.redirect("login");
+    } else {
+      passport.authenticate('local')(req, res, function() {
+        res.redirect("/");
+      });
+    }
+  });
+});
+
+
+
+app.get('/post/:postid', function(req, res) {
+  const requestedpostid=req.params.postid;
+  Post.findOne({_id:requestedpostid},function(err,posts){
+    res.render("post",{
+      postkatitle:posts.title,
+      postkacontent:posts.post});
+  });
+});
+
+app.listen(process.env.PORT, function() {
+  console.log("Server started on port 3000");
+});
